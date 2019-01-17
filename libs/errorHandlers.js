0dd26d0bece6993b8187f3fd9dcebf2e35724baa
MaxMitko <maxmitko@mail.ru>
2019-01-18 00:10:04 +0300

    feat(errorHandler): serialize validation failed error

--- libs/errorHandlers.js
+++ libs/errorHandlers.js
@@ -1,10 +1,12 @@
 const logger = require('./logger')
+const serializeError = require('serialize-error');
 
 module.exports.rootErrHandler = function (err, req, res, next) {
     logger.error(err)
     switch (err.message) {
         case ('Validation failed'):
-            res.status(422).json(err.mapped());
+            err.fields = err.mapped()
+            res.status(422).json(serializeError(err));
             break;
         case ('Not Acceptable'):
             res.status(406).send('Not Acceptable')
